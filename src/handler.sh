c5b6591499c9eaeba469c15ce1feafe68d368162
somniasum <somniasum@proton.me>
2026-01-17 12:25:45 +0200

    Merged gnome_shortcut into handler

--- src/handler.sh
+++ src/handler.sh
@@ -157,3 +157,22 @@ EOF
         log NOTICE "Systemd service configuration already exists [ ~/.config/systemd/user/kbd-backlight.service ]"
     fi
 }
+
+# Gnome shortcut configuration
+set_shortcut(){
+    # Configure the shortcuts
+    gsettings set org.gnome.settings-daemon.plugins.media-keys custom-keybindings "['/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom_on/', '/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom_off/']"
+
+    # On settings
+    log INFO "Configuring keyboard backlight on shortcut"
+    gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom_on/ name 'Keyboard Backlight on' && \
+    gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom_on/ command "/usr/local/bin/backlight.sh on" && \
+    gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom_on/ binding '<Super><Shift>Return'
+
+    # Off settings
+    log INFO "Configuring keyboard backlight off shortcut"
+    gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom_off/ name 'Keyboard Backlight off' && \
+    gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom_off/ command "/usr/local/bin/backlight.sh off" && \
+    gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom_off/ binding '<Super><Ctrl>Return'
+
+}
