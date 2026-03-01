d32b023805d84f4bfaa8f1106e598d5e3ba46bc7
somniasum <somniasum@proton.me>
2026-03-01 20:17:52 +0200

    log handling

--- src/alias.sh
+++ src/alias.sh
@@ -10,19 +10,20 @@ shell="${SHELL##*/}"
 
 ## Shell indentifier
 shell_set() {
-	case "$shell" in
-		bash)
-			echo "$home/.bashrc"
-			;;
-		fish)
-			echo "$home/.config/fish/config.fish"
-			;;
-		zsh)
-			echo "$home/.zshrc"
-			;;
-		*)
-			log ERROR "Current shell is unsupported. Please edit shell config manually."
-	esac
+  case "$shell" in
+  bash)
+    echo "$home/.bashrc"
+    ;;
+  fish)
+    echo "$home/.config/fish/config.fish"
+    ;;
+  zsh)
+    echo "$home/.zshrc"
+    ;;
+  *)
+    log ERROR "Current shell is unsupported. Please edit shell config manually."
+    ;;
+  esac
 }
 
 ## Set shell config name
@@ -31,17 +32,17 @@ shell_set_value=$(shell_set)
 ## To check if there is an alias matching that of the script
 shell_config() {
 
-    log INFO "Configuring $shell_set_value with needed aliases [on/off]"
+  log INFO "Configuring $shell_set_value with needed aliases [on/off]"
 
-	if ! grep -Eq "^alias (on|off)=" "$shell_set_value"; then
+  if ! grep -Eq "^alias (on|off)=" "$shell_set_value"; then
 
-		## Inserts the alias to the current shell being used
-		echo "#Alias for backlight" >> "$shell_set_value"
-		echo "alias on='$main_script_path/backlight.sh on'" >> "$shell_set_value"
-		echo "alias off='$main_script_path/backlight.sh off'" >> "$shell_set_value"
-		log SUCCESS "Aliases added to $shell_set_value."
+    ## Inserts the alias to the current shell being used
+    echo "#Alias for backlight" >>"$shell_set_value" && \ 
+    echo "alias on='$main_script_path/backlight.sh on'" >>"$shell_set_value" && \ 
+    echo "alias off='$main_script_path/backlight.sh off'" >>"$shell_set_value" && \ 
+    log SUCCESS "Aliases added to $shell_set_value." || log_error "Aliases failed to be set. Review [ $shell_set_value]."
 
-	else
-		log NOTICE "Alias [on/off] is already set. Please review $shell_set_value"
-	fi
+  else
+    log NOTICE "Alias [on/off] is already set."
+  fi
 }
